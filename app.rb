14e40d25babfca91b0f5154e82b550cb721d2cb0
krissasantos <krissa.santos31@gmail.com>
2016-10-16 22:57:02 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,19 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  get '/' do
+    "Hello, World!"
+  end
+
+   get '/name' do
+    "Hello, World! My name is Krissa"
+  end
+
+   get '/hometown' do
+    "Hello, World! My hometown is Cebu"
+  end
+
+   get '/favorite-song' do
+    "Hello, World! My favorite song is Everybody Wants to Rule the World by Tears for Fears"
+  end
 end
